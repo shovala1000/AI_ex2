1a83425ec134f62ac8676d7fe8189dcd873efa61
shoval <shovala1000@gmail.com>
2024-03-10 00:21:13 +0200

    Global vars init

--- ex2.py
+++ ex2.py
@@ -1,7 +1,40 @@
 import time
 import random
 
-id = ["000000000"]
+id = ["206626681"]
+
+""" Rules """
+WALL = 99
+PACMAN = 70
+RED_DOT = 21
+RED = 20
+BLUE_DOT = 31
+BLUE = 30
+YELLOW_DOT = 41
+YELLOW = 40
+GREEN_DOT = 51
+GREEN = 50
+DOT = 11
+EMPTY_SLOT = 10
+RIGHT = "R"
+LEFT = "L"
+DOWN = "D"
+UP = "U"
+
+""" Locations keys """
+KEY_PACMAN = 7
+KEY_GREEN = 5
+KEY_YELLOW = 4
+KEY_BLUE = 3
+KEY_RED = 2
+
+""" Invalid state """
+INVALID_STATE = "Error!!"
+
+""" Points """
+EAT_DOT = 1
+WIN = 10
+LOST = -10
 
 class Controller:
     "This class is a controller for a Pacman game."
