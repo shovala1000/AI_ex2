ab0edc5f4c6b71e0f7658c95c03f368012e0c420
shoval <shovala1000@gmail.com>
2024-03-11 20:36:39 +0200

    todo: continue the training part and then do the play

--- ex2.py
+++ ex2.py
@@ -1,3 +1,4 @@
+import math
 import time
 import random
 
@@ -38,6 +39,10 @@ EAT_DOT = 1
 WIN = 10
 LOST = -10
 
+""" Global vars """
+EPISODES = 1000
+IS_TRAINING = True
+
 
 class Controller:
     "This class is a controller for a Pacman game."
@@ -51,25 +56,18 @@ class Controller:
         init_locations - the locations of pellets in the initial state
         steps - number of steps the controller will perform
         """
-        self.alpha = 0.1
-        self.gamma = 0.9
-        self.epsilon = 0.1
-        self.numTraining = 100
-        self.episodesSoFar = 0  # Count the number of games we have played
-        self.score = 0  # current score
-        self.lastState = []  # last state
-        self.lastAction = []  # last action
-
         self.steps = steps
-
-        self.actions = {LEFT: (-1, 0), DOWN: (0, 1), RIGHT: (1, 0), UP: (0, -1)}
-        self.state = init_locations[KEY_PACMAN]
-        self.action = None  # Set initial action
-        self.Q = {}  # Initialize Q-table with zeros
-        self.init_q(N, M)
+        self.alpha = 0.8
+        self.gamma = 0.95
+        self.epsilon = 1
 
         # Initialize game environment for visualization
-        self.game = pacman.Game(steps, self.create_board(N, M, init_locations, init_pellets))
+        self.env = pacman.Game(steps, self.create_board(N, M, init_locations, init_pellets))
+        self.states = []  # last state
+        # self.actions = []  # last action
+
+        self.actions = {LEFT: (-1, 0), DOWN: (0, 1), RIGHT: (1, 0), UP: (0, -1)}
+        self.q_table = self.create_q_table(N, M)  # Initialize Q-table with zeros
 
     def create_board(self, N, M, init_locations, init_pellets):
         """
@@ -80,7 +78,8 @@ class Controller:
         """
         board = [[0] * M for _ in range(N)]
         for key, value in init_locations.items():
-            board[value[0]][value[1]] = key * 10
+            if value:
+                board[value[0]][value[1]] = key * 10
         for pellet in init_pellets:
             board[pellet[0]][pellet[1]] = 1
         # Print board
@@ -88,67 +87,52 @@ class Controller:
             print(str(row) + "\n")
         return board
 
-    def init_q(self, N, M):
+    def create_q_table(self, N, M):
         """
         Initialize Q-table and other necessary variables.
         """
+        q_table = {}
         for i in range(N):
             for j in range(M):
                 for action in self.actions.keys():
-                    self.Q[((i, j), action)] = 0
+                    q_table[((i, j), action)] = 0
+        return q_table
 
     def choose_next_move(self, locations, pellets):
         """
         Choose next action for Pacman given the current state of the board.
         """
-        state = locations[KEY_PACMAN]
-        possible_actions = list(self.actions.keys())
+        episode_rewards = []
+        episode_steps = []
 
-        # Explore with probability epsilon
-        if random.random() < self.epsilon:
-            return random.choice(possible_actions)
+        for episode in range(EPISODES):
+            self.env.reset()
+            s = self.env.board  # init state - todo: maybe in our case it should be the pacman location
+            episode_reward = 0
+            step = 0
+            while step < self.steps:
+                a = self.run_epsilon_greedy(self.q_table[s, :], self.actions.keys(), True, self.epsilon, episode)
 
-        # Exploit: choose action with the highest Q-value for the current state
+                # todo: continue the training part and then do the play
+
+    def find_max_action(self, q_table, state):
+        max_action = None
         max_q_value = float('-inf')
-        best_action = None
-        for action in possible_actions:
-            q_value = self.Q.get((state, action), 0)
+
+        for action in self.actions.keys():
+            q_value = q_table.get((state, action), 0)
             if q_value > max_q_value:
                 max_q_value = q_value
-                best_action = action
+                max_action = action
 
-        return best_action
+        return max_action
 
-
-
-    def update_q(self, state, action, reward, q_max):
-        """
-        update Q value
-        """
-        q = self.Q.get(state, action)
-        self.Q[(state, action)] = q + self.alpha * (reward + self.gamma * q_max - q)
-
-    def play_game(self):
-        """
-        Execute the game using the Q-learning algorithm.
-        """
-        for _ in range(self.steps):
-            # Choose next action
-            move = self.choose_next_move(self.game.locations.copy(), self.game.pellets.copy())
-
-            # Update state based on action
-            reward = self.game.update_board(self.actions[move])
-            new_state = self.game.locations[KEY_PACMAN]
-
-            # Update Q-value using Bellman equation
-            old_q_value = self.Q.get((self.state, self.action), 0)
-            max_future_q_value = max([self.Q.get((new_state, a), 0) for a in self.actions.keys()])
-            new_q_value = old_q_value + self.alpha * (reward + self.gamma * max_future_q_value - old_q_value)
-            self.Q[(self.state, self.action)] = new_q_value
-
-            # Update state and action for next iteration
-            self.state = new_state
-            self.action = move
-
-
-
+    def run_epsilon_greedy(self, arm_rewards, arms, is_training, epsilon, episode):
+        if is_training:
+            epsilon = math.exp(-0.01 * episode)
+        random_num = random.random()
+        if random_num > epsilon:
+            max_reward_arm = self.find_max_action(arm_rewards, (4, 0))
+        else:
+            max_reward_arm = random.choice(arms)
+        return max_reward_arm
